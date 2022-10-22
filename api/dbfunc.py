d7608225a95463b897e7a5adbf84f26dd4dc90ab
DongwonTuna <ttuna0790@gmail.com>
2022-10-22 12:27:37 +0900

    update

--- api/dbfunc.py
+++ api/dbfunc.py
@@ -8,13 +8,13 @@ def fetch_tickers(exchange):
                 data = cur.fetchall()
                 if len(data) == 0:
                     return {"status": 404, "data":"Exchange does not Found"}
-                return {"status":200, "data":{"exc": data[0][0],"tickers":data[0][1]}}
+                return {"status":200, "data":data[0][1]}
                 
 def fetch_exchanges():
      with psycopg.connect("dbname=API_SERVER user=postgres password=0790") as post:
             with post.cursor() as cur:
                 cur.execute("SELECT exchange FROM TICKERS")
-                return {"status":200, "data":{"excs":[db[0].upper().replace('-','').replace('_','').replace('/','') for db in cur.fetchall()]}}
+                return {"status":200, "data":[db[0].upper().replace('-','').replace('_','').replace('/','') for db in cur.fetchall()]}
 
 def fetch_chartdata(exchange, symbol):
          with psycopg.connect("dbname=API_SERVER user=postgres password=0790") as post:
@@ -24,4 +24,3 @@ def fetch_chartdata(exchange, symbol):
                 if len(data) == 0:
                     return {"status": 404, "data":"Ticker or Exchange does not Found"}
                 return {"status":200, "data":[[dta[1],dta[2],dta[3],dta[4],dta[5],dta[6]] for dta in data ]}
-                
